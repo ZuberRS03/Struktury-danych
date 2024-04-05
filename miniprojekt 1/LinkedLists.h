39538618cd2633dc468f0c7386e0051116409cf2
ZuberRS03 <matiz_z@o2.pl>
2024-04-05 20:02:23 +0200

    Poprawione LinkedLists

--- miniprojekt 1/LinkedLists.h
+++ miniprojekt 1/LinkedLists.h
@@ -4,53 +4,111 @@
 #include "Node.h"
 #include <iostream>
 
-
-
 template <typename T>
 class LinkedList {
 private:
     Node<T>* head; // Głowa listy, czyli pierwszy element
+    int size;
 
 public:
     // Konstruktor inicjalizujący listę jako pustą
-    LinkedList() : head(nullptr) {}
+    LinkedList() : head(nullptr), size(0) {}
 
     // Destruktor zwalniający pamięć
-    ~LinkedList() {
-        clear();
-    }
+    ~LinkedList();
 
     // Metoda dodająca element na początku listy
-    void addFront(T data) {
-        Node<T>* newNode = new Node<T>(data, head);
-        head = newNode;
-    }
+    void addFront(T data);
+
+    void addEnd(T data);
+
+    void addAt(T data, int index);
 
     // Metoda usuwająca element z początku listy
-    void removeFront() {
-        if (head != nullptr) {
-            Node<T>* temp = head;
-            head = head->next;
-            delete temp;
-        }
-    }
+    void removeFront();
 
     // Metoda wyświetlająca wszystkie elementy listy
-    void print() {
-        Node<T>* temp = head;
-        while (temp != nullptr) {
-            std::cout << temp->data << " ";
-            temp = temp->next;
-        }
-        std::cout << std::endl;
-    }
+    void print();
 
     // Metoda czyszcząca listę
-    void clear() {
-        while (head != nullptr) {
-            removeFront();
-        }
-    }
+    void clear();
 };
+
+template<typename T>
+LinkedList<T>::~LinkedList() {
+    clear();
+}
+
+template<typename T>
+void LinkedList<T>::addFront(T data) {
+    Node<T>* newNode = new Node<T>(data, head);
+    newNode->next = head;
+    head = newNode;
+    size++;
+}
+
+template<typename T>
+void LinkedList<T>::addEnd(T data) {
+    Node<T>* newNode = new Node<T>(data);
+    if (head == nullptr) {
+        head = newNode;
+    } else {
+        Node<T>* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+    size++;
+}
+
+template<typename T>
+void LinkedList<T>::addAt(T data, int index) {
+    if (index < 0 || index > size) {
+        throw std::out_of_range("Niepoprawny indeks");
+    }
+    if (index == 0) {
+        addFront(data);
+    } else if (index == size) {
+        addEnd(data);
+    } else {
+        Node<T>* newNode = new Node<T>(data);
+        Node<T>* temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+        size++;
+    }
+}
+
+template<typename T>
+void LinkedList<T>::removeFront() {
+    if (head != nullptr) {
+        Node<T>* temp = head;
+        head = head->next;
+        delete temp;
+        size--;
+    }
+}
+
+template<typename T>
+void LinkedList<T>::print() {
+    Node<T>* temp = head;
+    while (temp != nullptr) {
+        std::cout << temp->data << " ";
+        temp = temp->next;
+    }
+    std::cout << std::endl;
+}
+
+template<typename T>
+void LinkedList<T>::clear() {
+    while (head != nullptr) {
+        removeFront();
+    }
+}
+
 
 #endif // LINKEDLIST_H
--- miniprojekt 1/main.cpp
+++ miniprojekt 1/main.cpp
@@ -47,7 +47,7 @@ void wypelnijListy(ArrayList<int> listy[], int dane[], int liczbaElementow, int 
 
 int main() {
 
-    const int liczbaPomiarow = 50; // Liczba pomiarów
+    const int liczbaPomiarow = 2; // Liczba pomiarów
 
     //Wczytanie danych z pliku do pomocniczej tablicy
     fin.open("wylosowane_dane_500000.txt");
@@ -124,12 +124,86 @@ int main() {
             break;
         }
         case 2: {
-            cout << "Wybrano liste jednokierunkowa (head)" << endl;
-//            LinkedList<int> lista;
-//            for(int i = 0; i < ileElementow; i++){
-//                lista.addFront(losujLiczbe(min, max));
-//            }
-//            lista.print();
+            fout.open("wynikiLinkedList.txt"); //Otwarie pliku do zapisu wyników
+            fout << "Ilosc elementow startowych; sredni czas dodania elementu na poczatku [ms]; sredni czas dodania elementu na koncu [ms]; sredni czas dodania elementu w losowym miejscu [ms]"<< endl;
+
+            LinkedList<int> listy[liczbaPomiarow]; // Tworzenie tablicy list
+
+            //petla uzupelniajaca tablice listy
+            for(int startElementow = 0; startElementow < 10; startElementow += 1){
+
+
+                //Pomiary dodania elementu na poczatku
+                cout << "Początkowe listy" << endl;
+                for(int i = 0; i < liczbaPomiarow; i++) {
+                    listy[i] = LinkedList<int>();
+                    for(int j = 0; j < startElementow; j++) {
+                        listy[i].addEnd(DaneZPliku[j]);
+                    }
+                    listy[i].print();
+                }
+
+                double calkowityCzasNaPoczatku = 0.0;
+                cout << "Listy po dodaniu elementu na poczatku:" << endl;
+                for(int i = 0; i < liczbaPomiarow; i++) {
+                    auto start = high_resolution_clock::now();
+                    listy[i].addFront(5);
+                    auto stop = high_resolution_clock::now();
+                    calkowityCzasNaPoczatku += duration<double, milli>(stop - start).count();
+                    cout << "Czas: " << calkowityCzasNaPoczatku << endl;
+                    listy[i].print();
+                }
+
+                double sredniCzasNaPoczatku = calkowityCzasNaPoczatku / liczbaPomiarow; // Obliczanie średniego czasu
+
+                cout << "Początkowe listy" << endl;
+                for(int i = 0; i < liczbaPomiarow; i++) {
+                    listy[i] = LinkedList<int>();
+                    for(int j = 0; j < startElementow; j++) {
+                        listy[i].addEnd(DaneZPliku[j]);
+                    }
+                    listy[i].print();
+                }
+
+                double calkowityCzasNaKoncu = 0.0;
+                cout << "Listy po dodaniu elementu na koncu:" << endl;
+                for(int i = 0; i < liczbaPomiarow; i++) {
+                    auto start = high_resolution_clock::now();
+                    listy[i].addEnd(5);
+                    auto stop = high_resolution_clock::now();
+                    calkowityCzasNaKoncu += duration<double, milli>(stop - start).count();
+                    cout << "Czas: " << calkowityCzasNaKoncu << endl;
+                    listy[i].print();
+                }
+
+                double sredniCzasNaKoncu = calkowityCzasNaKoncu / liczbaPomiarow; // Obliczanie średniego czasu
+
+                cout << "Początkowe listy" << endl;
+                for(int i = 0; i < liczbaPomiarow; i++) {
+                    listy[i] = LinkedList<int>();
+                    for(int j = 0; j < startElementow; j++) {
+                        listy[i].addEnd(DaneZPliku[j]);
+                    }
+                    listy[i].print();
+                }
+
+                double calkowityCzasLosoweMiejsce = 0.0;
+                cout << "Listy po dodaniu elementu w losowym miejscu:" << endl;
+                for(int i = 0; i < liczbaPomiarow; i++) {
+                    int losoweMiejsce = losujLiczbe(0, startElementow);
+                    auto start = high_resolution_clock::now();
+                    listy[i].addAt(5,losoweMiejsce);
+                    auto stop = high_resolution_clock::now();
+                    calkowityCzasLosoweMiejsce += duration<double, milli>(stop - start).count();
+                    cout << "Czas: " << calkowityCzasLosoweMiejsce << endl;
+                    listy[i].print();
+                }
+                double sredniCzasLosoweMiejsce = calkowityCzasLosoweMiejsce / liczbaPomiarow; // Obliczanie średniego czasu
+                cout << startElementow << ";" << fixed << setprecision(6) << sredniCzasNaPoczatku << fixed << setprecision(6) << sredniCzasNaKoncu << fixed << setprecision(6) << sredniCzasLosoweMiejsce <<endl;
+            }
+
+            fout.close();
+
             break;
         }
         case 3: {
