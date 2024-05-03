647b2d25ed5bfab7fe6a77d3d51dfddad416fea4
ZuberRS03 <matiz_z@o2.pl>
2024-05-03 13:23:00 +0200

    szkic maina
    Na razie lece na strzelnice to zrobiłem tylko szkielet, potem pewnie jeszcze troche dorobie

--- miniprojekt 2/main.cpp
+++ miniprojekt 2/main.cpp
@@ -1,8 +1,98 @@
-#include <iostream>
+#include <iostream> //do obsługi strumieni wejścia/wyjścia
+#include <fstream> //do obsługi plików
+#include <chrono> //do mierzenia czasu
+#include <cstdlib> //do losowania liczb
+
 #include "MaxHeap.h"
 #include "AVLTree.h"
 
+using namespace std;
+
+ifstream fin;
+ofstream fout;
+
+//funkcja do wyboru kolejki
+int menuKolejki() {
+    int wybor;
+    cout << "----------Wybor kolejki priorytetowej----------\n";
+    cout << "1. Max Heap\n";
+    cout << "2. AVL Tree\n";
+    cout << "3. Exit\n";
+    cout << "-----------------------------------------------\n";
+    cout << "Wybor ";
+    cin >> wybor;
+    return wybor;
+}
+
+//funkcja losująca liczbę z przedziału <min, max>
+int losujLiczbe(int min, int max){
+    return rand() % (max - min + 1) + min;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //podatawowe dane do badan
+    const int liczbaPomiarow = 30; // Liczba pomiarów 30
+    const int liczbaElementow = 100000; // Max liczba elementów w stróktórze danych 100000
+    const int skokIlosciDanych = 5000; // Skok ilości danych 5000
+
+    //wybor kolejki
+    int wybranaKolejka = menuKolejki();
+
+    //Ewentualne wczytanie danych z pliku
+
+    switch (wybranaKolejka) {
+        case 1:{
+            //otwarcie pliku do zapisu wynikow
+            fout.open("wynikiMaxHeap.txt");
+            fout << "Liczba elementow;Czas insert(e,p);Czas extract-max();Czas find-max();Czas modify-key(e,p);Czas return-size()" << endl;
+
+            //badania dla MaxHeap
+            for(int startElementow = 0; startElementow < liczbaElementow; startElementow += skokIlosciDanych) {
+                //pomiar insert(e,p)
+
+                //pomiar extract-max()
+
+                //pomiar find-max()
+
+                //pomiar modify-key(e,p)
+
+                //pomiar return-size()
+
+            }
+
+            fout.close();
+            break;
+        }
+        case 2:{
+
+            fout.open("wynikiAVLTree.txt");
+            fout << "Liczba elementow;Czas insert(e,p);Czas extract-max();Czas find-max();Czas modify-key(e,p);Czas return-size()" << endl;
+
+            //badania dla AVLTree
+            for(int startElementow = 0; startElementow < liczbaElementow; startElementow += skokIlosciDanych) {
+                //pomiar insert(e,p)
+
+                //pomiar extract-max()
+
+                //pomiar find-max()
+
+                //pomiar modify-key(e,p)
+
+                //pomiar return-size()
+
+            }
+
+            fout.close();
+            break;
+        }
+        case 3:{
+            cout << "Koniec programu\n";
+            break;
+        }
+        defoult:{
+            cout << "Niepoprawny wybor\n";
+            break;
+        }
+    }
     return 0;
 }
