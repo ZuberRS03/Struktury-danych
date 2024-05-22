8e1b58939b2ba89ca57802da2087600dfabf0a89
ZuberRS03 <matiz_z@o2.pl>
2024-05-22 18:19:34 +0200

    miniprojekt 3 Initial commit

---
+++ miniprojekt 3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //podstawowa biblioteka wejścia/wyjścia
+#include <fstream> //do wczytywania i zapisywania plików
+#include <chrono> //do pomiaru czasu
+
+using namespace std;
+
+ifstream fin;
+ofstream fout;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
